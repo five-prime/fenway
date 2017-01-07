13dfdeb37eb7f42a13ad7afbe7fb43578d3f362b
5ive Prime Design <5iveprimedesign@gmail.com>
2017-01-07 14:09:49 -0800

    fixed team selection and js formatting

--- scripts/test.js
+++ scripts/test.js
@@ -2,12 +2,13 @@
 function clearFirst(){
 
   $('.a, .b, .c, .d, .e, .f, .g').removeClass('on');
+
 }
 
 //clear second digit
 function clearSecond(){
-  $('.h, .i, .j, .k, .l, .m, .n').removeClass('on');
 
+  $('.h, .i, .j, .k, .l, .m, .n').removeClass('on');
 
 }
 
@@ -17,7 +18,6 @@ function clearSecond(){
 function fdZero(){
 
   $('.a , .1,  .g, .4').addClass('on');
-
 
 }
 
@@ -34,6 +34,7 @@ function fdOne(){
 //first digit display two
 
 function fdTwo(){
+
   clearFirst();
 
   $('.a , .d,  .g, #b-4, #c-4, #e-1, #f-1, #g-1').addClass('on');
@@ -43,6 +44,7 @@ function fdTwo(){
 //first digit display three
 
 function fdThree(){
+
   clearFirst();
 
   $('.a , .d,  .g, #b-4, #c-4, #e-4, #f-4, #g-4').addClass('on');
@@ -53,16 +55,17 @@ function fdThree(){
 //first digit display four
 
 function fdFour(){
+
   clearFirst();
 
   $('.d, .4, #a-1, #b-1, #c-1').addClass('on');
-
 
 }
 
 //first digit display five
 
 function fdFive(){
+
   clearFirst();
 
   $('.a, .d, .g, #b-1, #c-1, #e-4, #f-4, #g-4').addClass('on');
@@ -70,6 +73,7 @@ function fdFive(){
 }
 
 function fdSix(){
+
   clearFirst();
 
   $('.a, .1, .d, .g, #e-4, #f-4, #g-4').addClass('on');
@@ -77,6 +81,7 @@ function fdSix(){
 }
 
 function fdSeven(){
+
   clearFirst();
 
   $('.a, .4').addClass('on');
@@ -84,58 +89,78 @@ function fdSeven(){
 }
 
 function fdEight(){
+
   clearFirst();
 
   $('.a, .d, .g, .1, .4').addClass('on');
 
-
 }
 
 function fdNine(){
+
   clearFirst();
 
   $('.a, .d, .4, #a-2, #a-3, #b-1, #c-1, #d-2, #d-3').addClass('on');
 
 }
 
-
 //second digit
 function sdZero(){clearSecond();
-  $('.h , .one,  .n, .four').addClass('on');}
+
+  $('.h , .one,  .n, .four').addClass('on');
+
+}
 
 function sdOne(){clearSecond();
-  $('.four').addClass('on');}
 
+  $('.four').addClass('on');
 
+}
 
 function sdTwo(){clearSecond();
+
   $('.h , .k,  .n, #i-four, #j-four, #l-one, #m-one, #h-one').addClass('on');
 
 }
+
 function sdThree(){clearSecond();
-$('.h , .k,  .n, #i-four, #j-four, #l-four, #m-four, #n-four').addClass('on');
+
+  $('.h , .k,  .n, #i-four, #j-four, #l-four, #m-four, #n-four').addClass('on');
+
 }
+
 function sdFour(){clearSecond();
+
   $('.k, .four, #h-one, #i-one, #j-one').addClass('on');
+
  }
 
 function sdFive(){clearSecond();
+
   $('.h, .k, .n, #i-one, #j-one, #l-four, #m-four, #n-four').addClass('on');
+
 }
 
 function sdSix(){clearSecond();
+
   $('.h, .one, .k, .n, #l-four, #m-four, #n-four').addClass('on');
 
 }
+
 function sdSeven(){clearSecond();
+
   $('.h, .four').addClass('on');
 
 }
+
 function sdEight(){clearSecond();
+
   $('.h, .k, .n, .one, .four').addClass('on');
 
 }
+
 function sdNine(){clearSecond();
+
   $('.h, .k, .four, #h-two, #h-three, #i-one, #j-one, #k-two, #k-three').addClass('on');
 
 }
--- scripts/tester.js
+++ scripts/tester.js
@@ -1,139 +1,258 @@
+//global variable
+
+var teamName = -1;
+
 $(document).ready(function(){
+
   $('#pitchinput').click(function(){
+
+    //get input
+
     pinput = document.getElementById('pitchinput').value;
+
+    //First Digit display
 
     //00 - 09
 
     if (pinput >= 0 && pinput <= 9){
+
       fdZero();
+
     }
+
     // 10 -19
 
     else if (pinput >= 10 && pinput <= 19){
 
+      fdOne();
 
-fdOne();
-}
-// 20 - 29
-else if (pinput >= 20 && pinput <= 29){
+    }
 
-fdTwo();
-}
-// 30 - 39
-else if(pinput >= 30 && pinput <= 39){
+    // 20 - 29
 
-fdThree();
-}
-// 40 - 49
-else if(pinput >= 40 && pinput <=49){
+    else if (pinput >= 20 && pinput <= 29){
 
-fdFour();
-}
-// 50 - 59
-else if(pinput >= 50 && pinput <= 59){
+      fdTwo();
 
-fdFive();
+    }
 
-}
-// 60 - 69
-else if(pinput >= 60 && pinput <= 69){
+    // 30 - 39
 
-fdSix();  }
-// 70 - 79
-else if(pinput >= 70 && pinput <= 79){
+    else if(pinput >= 30 && pinput <= 39){
 
-fdSeven();
+      fdThree();
 
-}
-// 80 - 89
-else if(pinput >= 80 && pinput <= 89){
+    }
 
-fdEight();
-}
-// 90 - 99
+    // 40 - 49
 
-else if(pinput >=90 && pinput <= 99){
+    else if(pinput >= 40 && pinput <=49){
 
+      fdFour();
 
-fdNine();}
-else{
-  console.log("oops");
+    }
 
-}
+    // 50 - 59
 
+    else if(pinput >= 50 && pinput <= 59){
 
-//begin second digit
+      fdFive();
 
+    }
 
-//single digits must be entered manually
+    // 60 - 69
 
-if(pinput ==="0"){
-sdZero();
-}
-else if(pinput === "1"){
-  sdOne();
-}
-else if(pinput === "2"){
-  sdTwo();
-}
-else if(pinput === "3"){
-  sdThree();
-}
-else if(pinput === "4"){
-  sdFour();
-}
-else if(pinput === "5"){
-  sdFive();
-}
-else if(pinput === "6"){
-  sdSix();
-}
-else if(pinput === "7"){
-  sdSeven();
-}
-else if(pinput === "8"){
-  sdEight();
-}
-else if(pinput === "9"){
-  sdNine();
-}
+    else if(pinput >= 60 && pinput <= 69){
+
+      fdSix();
+    }
+
+    // 70 - 79
+
+    else if(pinput >= 70 && pinput <= 79){
+
+      fdSeven();
+
+    }
+
+    // 80 - 89
+
+    else if(pinput >= 80 && pinput <= 89){
+
+      fdEight();
+    }
+
+    // 90 - 99
+
+    else if(pinput >=90 && pinput <= 99){
+
+      fdNine();
+    }
+
+    else{
+
+      console.log("error");
+
+    }
 
 
-//begin slice and analyze second digit
+    //begin second digit
+    //Single digits must be entered manually
 
-pinputSecond = pinput.slice(1,2);
+    if(pinput ==="0"){
 
-if(pinputSecond === '0'){
-sdZero();
-}
-else if(pinputSecond === '1'){
-sdOne();
-}
-else if(pinputSecond === '2'){
-sdTwo();
-}
-else if(pinputSecond === '3'){
-sdThree();
-}
-else if(pinputSecond === '4'){
-sdFour();
-}
-else if(pinputSecond === '5'){
-sdFive();
-}
-else if(pinputSecond === '6'){
-sdSix();
-}
-else if(pinputSecond === '7'){
-sdSeven();
-}
-else if(pinputSecond === '8'){
-sdEight();
-}
-else if(pinputSecond === '9'){
-sdNine();
-}
+      sdZero();
+    }
 
+    else if(pinput === "1"){
 
-});
+      sdOne();
+    }
+
+    else if(pinput === "2"){
+
+      sdTwo();
+
+    }
+
+    else if(pinput === "3"){
+
+      sdThree();
+
+    }
+
+    else if(pinput === "4"){
+
+      sdFour();
+
+    }
+
+    else if(pinput === "5"){
+
+      sdFive();
+
+    }
+
+    else if(pinput === "6"){
+
+      sdSix();
+
+    }
+
+    else if(pinput === "7"){
+
+      sdSeven();
+
+    }
+
+    else if(pinput === "8"){
+
+      sdEight();
+
+    }
+
+    else if(pinput === "9"){
+
+      sdNine();
+
+    }
+
+    //begin slice and analyze second digit
+
+    pinputSecond = pinput.slice(1,2);
+
+    if(pinputSecond === '0'){
+
+      sdZero();
+
+    }
+
+    else if(pinputSecond === '1'){
+
+      sdOne();
+
+    }
+
+    else if(pinputSecond === '2'){
+
+      sdTwo();
+
+    }
+
+    else if(pinputSecond === '3'){
+
+      sdThree();
+
+    }
+
+    else if(pinputSecond === '4'){
+
+      sdFour();
+
+    }
+
+    else if(pinputSecond === '5'){
+
+      sdFive();
+
+    }
+
+    else if(pinputSecond === '6'){
+
+      sdSix();
+
+    }
+
+    else if(pinputSecond === '7'){
+
+      sdSeven();
+
+    }
+
+    else if(pinputSecond === '8'){
+
+      sdEight();
+
+    }
+
+    else if(pinputSecond === '9'){
+
+      sdNine();
+
+    }
+//end click function
+  });
+
+//change visiting team name by clicking on cell
+
+$('#visitor').click(function(){
+
+  $('#visitor').css("background-color", "inherit");
+
+  $('#visitor').css("border","1px solid gray");
+
+  //list of teams stored as variable
+
+  var teams =["BAL","TOR","NYY","TB ","CLE","DET","KC ","CWS","MIN","TEX","SEA","HOU","LAA","OAK","WAS","NYM","MIA","PHI","ATL","CHC","STL","PIT","MIL","CIN","LAD","SF ","COL","ARI","SD "];
+
+  //30 teams, first index position is 0 so teams = 29
+
+  //alphabetize
+
+  teams.sort();
+
+  if (teamName < 29){
+
+    teamName += 1;
+
+    $(this).html(teams[teamName]);
+                      }
+  else if (teamName >= 29){
+
+    teamName = -1;
+
+  }
+
+    });
+//end doc ready function
 });
